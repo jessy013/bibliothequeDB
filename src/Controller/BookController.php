e4c6e47bf8cf610becefabfd6e4a4e997cec403b
jessy013 <84912053+jessy013@users.noreply.github.com>
2021-07-30 16:32:59 +0200

    up

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controller;
-use App\Controller\BookService;
+use app\service\BookService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
